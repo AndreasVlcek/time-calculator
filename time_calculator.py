88eaf37adcfccb35e8ab6910065cf21778c8d3ed
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2023-03-27 21:27:15 +0200

    started with time calculator

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,18 @@
 def add_time(start, duration):
+    
+    return_time = ''
 
+    start_time_split = start.split(' ')
+    start_hours = int(start_time_split[0].split(':')[0])
+    start_minutes = int(start_time_split[0].split(':')[1])
+    am_pm = start_time_split[1]
 
+    print(start_hours)
+    print(start_minutes)
+    print(am_pm)
 
+    return return_time
 
-
+    # TODO
+    
     return new_time
