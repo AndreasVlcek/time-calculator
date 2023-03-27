d9f91e7e4ea88a2c4bf65fc4c1230149e6a6d7b8
ANDREAS VLCEK <andreas.vlcek@googlemail.com>
2023-03-27 21:51:16 +0200

    starting calculation

--- time_calculator.py
+++ time_calculator.py
@@ -1,22 +1,25 @@
 def add_time(start, duration, weekday=False):
 
-    return_time = ''
+  return_time = ''
 
-    start_time_split = start.split(' ')
-    start_hours = int(start_time_split[0].split(':')[0])
-    start_minutes = int(start_time_split[0].split(':')[1])
-    am_pm = start_time_split[1]
+  # split the start time
+  start_time_split = start.split(' ')
+  start_hours = int(start_time_split[0].split(':')[0])
+  start_minutes = int(start_time_split[0].split(':')[1])
+  am_pm = start_time_split[1]
 
-    print(start_hours)
-    print(start_minutes)
-    print(am_pm)
+  print(start_hours)
+  print(start_minutes)
+  print(am_pm)
 
-    duration_hours = int(duration.split(':')[0])
-    duration_minutes = int(duration.split(':')[1])
+  # split the duration
+  duration_hours = int(duration.split(':')[0])
+  duration_minutes = int(duration.split(':')[1])
 
-    print(duration_hours)
-    print(duration_minutes)
-    
-    # TODO
+  print(duration_hours)
+  print(duration_minutes)
 
-    return return_time
+  # add duration minutes to start minutes
+  minutes = start_minutes + duration_minutes
+
+  return return_time
